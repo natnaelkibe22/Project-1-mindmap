c7c8287ad81b50cfed228f63c9348e5fb0ff97b8
Rosemary Charnley <rosemary.charnley001@umb.edu>
2020-11-25 18:52:39 +0000

    Update init.sql to support two new databases

--- init.sql
+++ init.sql
@@ -1,8 +1,25 @@
-CREATE TABLE mindmap (
-  center VARCHAR(1000) NOT NULL,
-  topic VARCHAR(1000),
-  subtopic VARCHAR(1000)
+/*
+one-to-many relationship
+*/
+CREATE TABLE mindmaps
+(
+   name    varchar(100) primary key not null,
+   topic   varchar(3000) not null,
+   shape   varchar(100) not null
 );
 
-INSERT INTO mindmap (center, topic, subtopic)
-VALUES  (' TEST CENTER', ' TEST TOPIC', 'TES SUBTOPIC');
+create table children
+(
+  name    varchar(100) primary key not null,
+  parent  varchar(100) not null,
+  topic   varchar(3000) not null,
+  shape   varchar(100) not null,
+  FOREIGN KEY (parent) REFERENCES mindmaps(name)
+);
+
+INSERT INTO mindmaps (name, topic, shape)
+VALUES  ('ANIMALS', 'classification of animals', 'ellipse');
+
+INSERT INTO children (name, parent, topic, shape)
+VALUES  ('MAMMAL', 'ANIMALS', 'a warm-blooded vertebrate animal of a class that is distinguished by the possession of hair or fur, the secretion of milk by females for the nourishment of the young, and (typically) the birth of live young.', 'ellipse');
+
