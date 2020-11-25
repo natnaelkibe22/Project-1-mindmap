606bd1e03999f64f66bb89deddd023d86b32223e
Rosemary Charnley <rosemary.charnley001@umb.edu>
2020-11-25 18:51:40 +0000

    Update index.js to support two new tables, routes not complete

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
 
-const getMindmaps = (request, response) => {
+//GET all mindmaps
+const GETMindmaps = (request, response) => {
   pool.query('SELECT * FROM mindmaps', (error, results) => {
     if (error) {
       throw error
@@ -18,17 +19,42 @@ const getMindmaps = (request, response) => {
   })
 }
 
-const addMindmap = (request, response) => {
-  const {center, topic, subtopic} = request.body
+//GET all children
+const GETChildren = (request, response) => {
+  pool.query('SELECT * FROM children', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
+//POST Mindmap
+const POSTMindmap = (request, response) => {
+  const {name, topic, shape} = request.body
   pool.query(
-    'INSERT INTO mindmaps (center, topic, subtopic) VALUES ($1, $2, $3)',
-    [center, topic, subtopic],
+    'INSERT INTO mindmaps (name, topic, shape) VALUES ($1, $2, $3)',
+    [name, topic, shape],
     (error) => {
       if (error) {
         throw error
       }
       response.status(201).json({status: 'success', message: 'Mindmap added.'})
+    },)
+}
+
+//POST Children
+const POSTChildren = (request, response) => {
+  const {name, parent, topic, shape} = request.body
+
+  pool.query(
+    'INSERT INTO children (name, parent, topic, shape) VALUES ($1, $2, $3, $4)',
+    [name, parent, topic, shape],
+    (error) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).json({status: 'success', message: 'Child added.'})
     },
   )
 }
@@ -36,9 +62,15 @@ const addMindmap = (request, response) => {
 app
   .route('/mindmaps')
   // GET endpoint
-  .get(getMindmaps)
+  .get(GETMindmaps)
   // POST endpoint
-  .post(addMindmap)
+  .post(POSTMindmap)
+  //HOW DO YOU SUPPORT MULTIPLE ROUTES IN THE SAME APP??
+  //.route('/mindmaps/children')
+  // GET endpoint
+  //.get(GETChildren)
+  // POST endpoint
+  //.post(POSTChildren)
 
 // Start server
 app.listen(process.env.PORT || 3002, () => {
