a8641348b26af717b5e341ca5c345eb2e608a038
Rosemary Charnley <rosemary.charnley001@umb.edu>
2020-12-01 22:49:34 +0000

    Update init.sql adding additional test cases

--- init.sql
+++ init.sql
@@ -18,7 +18,7 @@ CREATE TABLE nodes
   /* if node is directly attached to mindmap entity,
   parent will be the ID of the node itself, meaning parent == id
   Code checks for this in the endpoints will be important for
-  correct functionality*/
+  correct functionality */
   parent  varchar(100) not null,
   mindmap varchar(100) not null,
   topic   varchar(3000) not null,
@@ -27,8 +27,15 @@ CREATE TABLE nodes
   FOREIGN KEY (parent) REFERENCES nodes(id)
 );
 
+/* Test values */
 INSERT INTO mindmaps (id, name, topic, shape)
 VALUES  ('123abc', 'ANIMALS', 'animal types', 'rectangle');
 
 INSERT INTO nodes (id, name, parent, mindmap, topic, shape)
 VALUES  ('456efg','MAMMAL', '456efg', '123abc', 'a warm-blooded vertebrate animal of a class that is distinguished by the possession of hair or fur, the secretion of milk by females for the nourishment of the young, and (typically) the birth of live young.', 'ellipse');
+
+INSERT INTO nodes (id, name, parent, mindmap, topic, shape)
+VALUES  ('555aaa','DOG', '456efg', '123abc', 'woof woof dogs have 4 legs', 'ellipse');
+
+INSERT INTO nodes (id, name, parent, mindmap, topic, shape)
+VALUES  ('678rrr','YELLOW LAB', '555aaa', '123abc', 'The yellow Lab is perhaps best known for its role as a guide dog for the blind.', 'ellipse');
