66015fa13fe9b49f9c31817e0ddd9a5092fb9b39
Rosemary Charnley <rosemary.charnley001@umb.edu>
2020-12-01 22:44:26 +0000

    Update init.sql so nodes table does not support null parents, instead self referencing

--- init.sql
+++ init.sql
@@ -4,7 +4,7 @@ one-to-many relationship
 CREATE TABLE mindmaps
 (
    /* 6 character string of alphanumberic */
-   id      varchar(6) not null primary key not null,
+   id      varchar(6) primary key not null,
    name    varchar(100) not null,
    topic   varchar(3000) not null,
    shape   varchar(100) not null
@@ -15,9 +15,11 @@ CREATE TABLE nodes
   /* 6 character string of alphanumberic */
   id      varchar(6) primary key not null,
   name    varchar(100) not null,
-  /* if node is directly attached to mindmap entity, parent will be null and
-  only the mindmap field will contain a value*/
-  parent  varchar(100),
+  /* if node is directly attached to mindmap entity,
+  parent will be the ID of the node itself, meaning parent == id
+  Code checks for this in the endpoints will be important for
+  correct functionality*/
+  parent  varchar(100) not null,
   mindmap varchar(100) not null,
   topic   varchar(3000) not null,
   shape   varchar(100) not null,
@@ -29,4 +31,4 @@ INSERT INTO mindmaps (id, name, topic, shape)
 VALUES  ('123abc', 'ANIMALS', 'animal types', 'rectangle');
 
 INSERT INTO nodes (id, name, parent, mindmap, topic, shape)
-VALUES  ('456efg','MAMMAL', NULL, '123abc', 'a warm-blooded vertebrate animal of a class that is distinguished by the possession of hair or fur, the secretion of milk by females for the nourishment of the young, and (typically) the birth of live young.', 'ellipse');
+VALUES  ('456efg','MAMMAL', '456efg', '123abc', 'a warm-blooded vertebrate animal of a class that is distinguished by the possession of hair or fur, the secretion of milk by females for the nourishment of the young, and (typically) the birth of live young.', 'ellipse');
