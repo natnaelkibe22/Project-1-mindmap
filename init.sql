5f25e2322180b2764d8f6a963932eb5dba809894
Rosemary Charnley <rosemary.charnley001@umb.edu>
2020-12-02 16:50:01 +0000

    Update init.sql to make ID unique in table

--- init.sql
+++ init.sql
@@ -4,16 +4,19 @@ one-to-many relationship
 CREATE TABLE mindmaps
 (
    /* 6 character string of alphanumberic */
-   id      varchar(6) primary key not null,
+   id      varchar(6) primary key not null unique,
+   /*topic */
    name    varchar(100) not null,
+   /*description */
    topic   varchar(3000) not null,
    shape   varchar(100) not null
+   /* color? */
 );
 
 CREATE TABLE nodes
 (
   /* 6 character string of alphanumberic */
-  id      varchar(6) primary key not null,
+  id      varchar(6) primary key not null unique,
   name    varchar(100) not null,
   /* if node is directly attached to mindmap entity,
   parent will be the ID of the node itself, meaning parent == id
@@ -23,6 +26,7 @@ CREATE TABLE nodes
   mindmap varchar(100) not null,
   topic   varchar(3000) not null,
   shape   varchar(100) not null,
+  /* color? */
   FOREIGN KEY (mindmap) REFERENCES mindmaps(id),
   FOREIGN KEY (parent) REFERENCES nodes(id)
 );
@@ -39,3 +43,4 @@ VALUES  ('555aaa','DOG', '456efg', '123abc', 'woof woof dogs have 4 legs', 'elli
 
 INSERT INTO nodes (id, name, parent, mindmap, topic, shape)
 VALUES  ('678rrr','YELLOW LAB', '555aaa', '123abc', 'The yellow Lab is perhaps best known for its role as a guide dog for the blind.', 'ellipse');
+
